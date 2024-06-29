b1bae215db565706ef74d2b86998cd5bd2c99270
SuwasG <suwasghale693@gmail.com>
2024-06-29 08:33:28 +0545

    labels and their icons

--- login.py
+++ login.py
@@ -23,8 +23,34 @@ class LoginWindow:
         self.lbl_img1 = Label(image=self.photo_img1, bg="yellow", borderwidth=0)
         self.lbl_img1.place(x=620, y=155, width=100, height=100)
 
-        get_started = Label(self.frame, text="Welcome! Get Started.", font=("times new roman", 20, "bold"), fg="black", bg="yellow")
-        get_started.place(x=125, y=105)
+        get_started = Label(self.frame, text="Welcome", font=("times new roman", 25, "bold"), fg="black", bg="yellow")
+        get_started.place(x=100, y=105)
+
+                # labels
+        user_name = Label(self.frame, text='User Name',font=("times new roman", 15, "bold"), fg="black", bg="yellow")
+        user_name.place(x=70, y=150)
+
+        self.txt_user = ttk.Entry(self.frame, font=("times new roman", 15, "bold"))
+        self.txt_user.place(x=40, y=180, width=270)
+
+        password = Label(self.frame, text='Password',font=("times new roman", 15, "bold"), fg="black", bg="yellow")
+        password.place(x=70, y=210)
+
+        self.txt_pass = ttk.Entry(self.frame, font=("times new roman", 15, "bold"), ) # show="*"
+        self.txt_pass.place(x=40, y=240, width=270)
+
+        # Icon Images
+        img2= Image.open(r"C:\Users\Acer\Desktop\projects_2024\tkinter\login\images\loginIcon.png")
+        img2=img2.resize((25,25), Image.LANCZOS)
+        self.photo_img2= ImageTk.PhotoImage(img2)
+        self.lbl_img2 = Label(image=self.photo_img2, bg="yellow", borderwidth=0)
+        self.lbl_img2.place(x=540, y=300, width=25, height=25)
+
+        img3= Image.open(r"C:\Users\Acer\Desktop\projects_2024\tkinter\login\images\pass.png")
+        img3=img3.resize((25,25), Image.LANCZOS)
+        self.photo_img3= ImageTk.PhotoImage(img3)
+        self.lbl_img3 = Label(image=self.photo_img3, bg="yellow", borderwidth=0)
+        self.lbl_img3.place(x=540, y=360, width=25, height=25)
 
 
 if __name__ == '__main__':
