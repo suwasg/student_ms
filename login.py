9cd708bf470e2f91f812a92961462cf0a3243e55
SuwasG <suwasghale693@gmail.com>
2024-06-29 08:29:17 +0545

    img1 and get started text

--- login.py
+++ login.py
@@ -17,6 +17,15 @@ class LoginWindow:
         self.frame.place(x=500, y=150, width=340, height=450)
 
 
+        img1= Image.open(r"images\loginIcon.png")
+        img1=img1.resize((100,100), Image.LANCZOS)
+        self.photo_img1= ImageTk.PhotoImage(img1)
+        self.lbl_img1 = Label(image=self.photo_img1, bg="yellow", borderwidth=0)
+        self.lbl_img1.place(x=620, y=155, width=100, height=100)
+
+        get_started = Label(self.frame, text="Welcome! Get Started.", font=("times new roman", 20, "bold"), fg="black", bg="yellow")
+        get_started.place(x=125, y=105)
+
 
 if __name__ == '__main__':
     root = Tk()
