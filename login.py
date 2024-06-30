429581b07453e7b4ad612a92f312dd794e287096
SuwasG <suwasghale693@gmail.com>
2024-06-30 09:45:14 +0545

    Added login.py to the login branch

--- login.py
+++ login.py
@@ -10,34 +10,39 @@ except ImportError:
     StudentManagementSystem = None
 class LoginWindow:
     def __init__(self, root):
+        # Set the root window and its properties
         self.root = root
         self.root.title("Login")
         self.root.geometry("1400x750+0+0")
 
+        # Load and display background image
         self.bg = ImageTk.PhotoImage(file=r'C:\Users\Acer\Desktop\projects_2024\tkinter\login\images\college1.png')
         self.lbl_bg = Label(self.root, image=self.bg)
         self.lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # Create a frame for the login section
         self.frame = Frame(self.root, bg='yellow')
         self.frame.place(x=500, y=180, width=340, height=350)
 
-
+        # Load and display login icon image
         img1= Image.open(r"images\loginIcon.png")
         img1=img1.resize((100,100), Image.LANCZOS)
         self.photo_img1= ImageTk.PhotoImage(img1)
         self.lbl_img1 = Label(image=self.photo_img1, bg="yellow", borderwidth=0)
         self.lbl_img1.place(x=620, y=185, width=100, height=100)
-
+        
+        # Label for "Welcome" text
         get_started = Label(self.frame, text="Welcome", font=("times new roman", 25, "bold"), fg="black", bg="yellow")
         get_started.place(x=100, y=105)
 
-                # labels
+        # user label
         user_name = Label(self.frame, text='User Name',font=("times new roman", 15, "bold"), fg="black", bg="yellow")
         user_name.place(x=70, y=150)
 
         self.txt_user = ttk.Entry(self.frame, font=("times new roman", 15, "bold"))
         self.txt_user.place(x=40, y=180, width=270)
 
+        # password label
         password = Label(self.frame, text='Password',font=("times new roman", 15, "bold"), fg="black", bg="yellow")
         password.place(x=70, y=210)
 
@@ -57,9 +62,11 @@ class LoginWindow:
         self.lbl_img3 = Label(image=self.photo_img3, bg="yellow", borderwidth=0)
         self.lbl_img3.place(x=540, y=390, width=25, height=25)
 
+        # login button
         login_btn = Button(self.frame, text="Login",command=self.login, font=("times new roman", 15 , "bold"),fg='white', bg='blue', bd=3, relief=RIDGE, activeforeground="white", activebackground="blue")
         login_btn.place(x=110, y=300, width=120)
 
+    # login function
     def login(self):
         "Login Functionality"
         # Load credentials from json file:
